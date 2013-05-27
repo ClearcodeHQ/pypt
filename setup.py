65a07d5ddc12acf3da4d47076a6bc6f71a88ef9f
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2013-05-27 18:47:40 +0200

    added setup files and travis.yml for ci

---
+++ setup.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from setuptools import setup, find_packages
+
+here = os.path.dirname(__file__)
+with open(os.path.join(here, 'namespace', 'package', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+
+def read(fname):
+    return open(os.path.join(here, fname)).read()
+
+requirements = []
+
+test_requires = [
+    'WebTest',
+    'nose',
+    'coverage',
+]
+
+extras_require = {
+    'docs': ['sphinx', 'sphinx_bootstrap_theme'],
+    'tests': test_requires
+}
+
+setup(
+    name='namespace.package',
+    version=package_version,
+    description='It\'s a namespace package tempalte only',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    keywords='python template',
+    author='Grzegorz Sliwinski',
+    author_email='username: fizyk, domain: fizyk.net.pl',
+    url='https://github.com/fizyk/pyramid_routing',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=find_packages(),
+    namespace_packages=['namespace'],
+    install_requires=requirements,
+    tests_require=test_requires,
+    test_suite='tests',
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=extras_require,
+)
